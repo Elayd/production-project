961cdece4fe4781eb61633b1fc8790b44e90d23d
Elayd <iamvine@mail.ru>
2023-01-30 21:24:36 +0300

    initial commit

---
+++ src/global.d.ts
@@ -0,0 +1,9 @@
+// Глобальная деклорация типов
+
+declare module '*.scss' {
+    interface IClassNames {
+        [className: string]: string
+    }
+    const classNames: IClassNames;
+    export = classNames;
+}
